a2b6534d331d5b212d00becc6abb7d33d1ff7e3f
TheusMarqs <matheusmarqsilva@gmail.com>
2023-10-20 16:48:34 -0300

    inicio angular

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
+import { ProfessorComponent } from './professor/professor.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { StudentComponent } from './student/student.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'professor', component: ProfessorComponent},
+  {path: 'student', component: StudentComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ProfessorComponent } from './professor/professor.component';
+import { StudentComponent } from './student/student.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ProfessorComponent,
+    StudentComponent
   ],
   imports: [
     BrowserModule,
